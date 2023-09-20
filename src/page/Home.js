b7ed3d75ec0296d25d6cb6380dbf0b3f9d63f3e6
ngothanhcong2882 <ntc2882@gmail.com>
2023-09-20 15:06:08 +0700

    cong note 2
    new2

---
+++ src/page/Home.js
@@ -0,0 +1,20 @@
+import { Input } from 'antd'
+import { Header } from 'antd/es/layout/layout'
+import React from 'react'
+
+const Home = () => {
+  return (
+
+    
+   
+    
+
+    <div className="flex flex-col w-full h-max bg-white justify-between">
+         
+          <h1>hello home</h1>
+          <Input style={{width: '100px'}} placeholder="Basic usage" />
+        </div>
+  )
+}
+
+export default Home
